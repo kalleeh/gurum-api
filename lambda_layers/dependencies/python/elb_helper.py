e47be53664ff40175917a77e8e783b5fd77e4093
kalleh <wallbomk@amazon.com>
2019-10-17 17:33:54 +0200

    fix: priority clash when creating multiple apps quickly

--- lambda_layers/dependencies/python/elb_helper.py
+++ lambda_layers/dependencies/python/elb_helper.py
@@ -9,28 +9,18 @@ or other written agreement between Customer and either
 Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 """
 
+import random
 import boto3
 
-from aws_xray_sdk.core import patch_all
 from logger import configure_logger
 
 import config
-
-patch_all()
 
 LOGGER = configure_logger(__name__)
 
 
 def get_next_rule_priority(listener_arn):
     """ Returns the next rule priority number for a given ALB Listener Arn
-
-    Args:
-        listener_arn (string): String of the ARN to the ALB Listener
-    Basic Usage:
-        >>> resp = iterate_rule_priority(listener_arn)
-    Returns:
-        Number: Number of the next available rule priority number
-        Default: 1
     """
     client = boto3.client('elbv2', region_name=config.PLATFORM_REGION)
     rules = {}
@@ -52,3 +42,29 @@ def get_next_rule_priority(listener_arn):
     priority = int(sorted_rules[0]['Priority'])+1
 
     return priority
+
+def get_random_rule_priority(listener_arn):
+    """ Returns a random available rule priority number for a given ALB Listener Arn
+    """
+    client = boto3.client('elbv2', region_name=config.PLATFORM_REGION)
+    rules = {}
+
+    try:
+        rules = client.describe_rules(
+            ListenerArn=listener_arn,
+        )['Rules']
+    except Exception as ex:
+        LOGGER.exception(ex)
+        raise
+
+    priorities = [int(rule['Priority']) for rule in rules if rule['Priority'].isdigit()]
+
+    if not priorities:
+        return 1
+
+    allocated_set = set(priorities)
+    possible_range_set = set(range(50000))
+    available_range_set = possible_range_set.difference(allocated_set)
+    random_rule_priority = random.sample(available_range_set, 1)[0]
+
+    return random_rule_priority
---
+++ lambda_layers/dependencies/python/tests/elb_helper_test.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+# pylint: skip-file
+
+from pytest import raises
+from datetime import datetime
+import elb_helper
+import random
+
+listener_arn = 'arn:aws:elasticloadbalancing:eu-west-1:789073296014:listener/app/gurum-platform/4ee4cd0d1fc306f9/d20a70244b82ce4e'
+
+def test_get_next_rule_priority():
+    result = elb_helper.get_next_rule_priority(listener_arn)
+    assert result == 0
+
+def test_get_random_rule_priority():
+    result = elb_helper.get_random_rule_priority(listener_arn)
+    assert result >= 0 and result < 50000
