f52c6e43ea266575bec12e8c9de52d2541ff57d8
Karl Wallbom <wallbomk@amazon.com>
2020-02-13 16:57:41 +0100

    feat: add group creation and email attribute

---
+++ helpers/cognito_quick_group.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+echo -e "Checking if the platform has been setup..\n"
+
+## Retreive Cognito Details
+USER_POOL_ID=$(aws cognito-idp list-user-pools --max-results 20 | jq -r '.UserPools[] | select(.Name == "gurum_users") | .Id')
+if [ -z $USER_POOL_ID ]; then 
+    echo "No user pool found. Ensure the platform has been setup first."
+    exit 1
+fi
+
+##App client id
+IDENTITY_POOL_ID=$(aws cognito-identity list-identity-pools --max-results 20 | jq -r '.IdentityPools[] | select(.IdentityPoolName == "gurum_idp") | .IdentityPoolId')
+if [ -z $IDENTITY_POOL_ID ]; then 
+    echo "No identity pool found. Ensure the platform has been setup first."
+    exit 1
+fi
+
+# MODIFY TRUST POLICY JSON
+MYDIR="$(dirname "$(which "$0")")"
+sed -i.bak "s/###REPLACEIDENTITYPOOLID###/$IDENTITY_POOL_ID/g" $MYDIR/group_trust_policy.json
+
+# USER CREATION
+echo "Enter a group name:"
+read GROUP_NAME
+
+## Create the IAM Role
+ROLE_NAME="gurum-$GROUP_NAME-role"
+ROLE_ARN=$(aws iam create-role \
+    --path '/gurum/groups/' \
+    --role-name $ROLE_NAME \
+    --assume-role-policy-document file://$MYDIR/group_trust_policy.json \
+    --description "Gurum Cognito Group Assume Role for $GROUP_NAME" \
+    --tags Key=gurum-groups,Value=$GROUP_NAME | jq -r '.Role.Arn')
+
+## Attach IAM role policy
+ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+aws iam attach-role-policy \
+    --role-name $ROLE_NAME \
+    --policy-arn "arn:aws:iam::$ACCOUNT_ID:policy/gurum/gurum-group-policy"
+
+## Create the Cognito Group
+aws cognito-idp create-group \
+    --group-name $GROUP_NAME \
+    --user-pool-id $USER_POOL_ID \
+    --role-arn $ROLE_ARN > /dev/null
+
+echo -e "\n\nSuccess! Group created and mapped to an IAM role."
--- helpers/cognito_quick_user.sh
+++ helpers/cognito_quick_user.sh
@@ -4,14 +4,14 @@ set -e
 echo -e "Checking if the platform has been setup..\n"
 
 ## Retreive Cognito Details
-POOL_ID=$(aws cognito-idp list-user-pools --max-results 20 | jq -r '.UserPools[] | select(.Name == "gurum_users") | .Id')
-if [ -z $POOL_ID ]; then 
+USER_POOL_ID=$(aws cognito-idp list-user-pools --max-results 20 | jq -r '.UserPools[] | select(.Name == "gurum_users") | .Id')
+if [ -z $USER_POOL_ID ]; then 
     echo "No user pool found. Ensure the platform has been setup first."
     exit 1
 fi
 
 ##App client id
-CLIENT_ID=$(aws cognito-idp list-user-pool-clients --user-pool-id $POOL_ID | jq -r '.UserPoolClients[] | select(.ClientName == "gurum-client") | .ClientId')
+CLIENT_ID=$(aws cognito-idp list-user-pool-clients --user-pool-id $USER_POOL_ID | jq -r '.UserPoolClients[] | select(.ClientName == "gurum-client") | .ClientId')
 if [ -z $CLIENT_ID ]; then 
     echo "No client id found. Ensure the platform has been setup first."
     exit 1
@@ -21,21 +21,24 @@ fi
 echo "Enter a username:"
 read USERNAME
 
+echo "Enter a valid e-mail:"
+read EMAIL
+
 echo -e "\nEnter a password:"
 read -s PASSWORD
 
 ## Create the Cognito user
-aws cognito-idp sign-up --client-id $CLIENT_ID --username $USERNAME --password $PASSWORD > /dev/null
+aws cognito-idp sign-up --client-id $CLIENT_ID --username $USERNAME --password $PASSWORD --user-attributes Name="email",Value="$EMAIL" > /dev/null
 
 ## Assign cognito user to group
-GROUP_NAMES=$(aws cognito-idp list-groups --user-pool-id $POOL_ID | jq -r '.Groups | map(.GroupName) | join(" , ")')
+GROUP_NAMES=$(aws cognito-idp list-groups --user-pool-id $USER_POOL_ID | jq -r '.Groups | map(.GroupName) | join(" , ")')
 
 echo -e "\n\nEnter a group for the user (valid: $GROUP_NAMES):"
 read SELECTED_GROUP
 
-aws cognito-idp admin-add-user-to-group --username $USERNAME --user-pool-id $POOL_ID --group-name $SELECTED_GROUP
+aws cognito-idp admin-add-user-to-group --username $USERNAME --user-pool-id $USER_POOL_ID --group-name $SELECTED_GROUP
 
 ## Confirm the user account
-aws cognito-idp admin-confirm-sign-up --username $USERNAME --user-pool-id $POOL_ID
+aws cognito-idp admin-confirm-sign-up --username $USERNAME --user-pool-id $USER_POOL_ID
 
 echo -e "\n\nSuccess! Log-in with your chosen details."
