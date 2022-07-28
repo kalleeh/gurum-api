5a6c5b48345b0f2edd8e7264732230f6d54b4f06
Karl Wallbom <karl.wallbom@gmail.com>
2022-07-28 10:15:19 +0200

    cleaning

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,18 @@ S3_BUCKET="gurum-deployment-artifacts-$ACCOUNT_ID-$REGION"
 if aws s3 ls "s3://$S3_BUCKET" 2>&1 | grep -q 'NoSuchBucket'; then
     echo "Creating initial artifacts bucket"
     aws s3 mb "s3://$S3_BUCKET"
+    aws s3api put-bucket-encryption --bucket $S3_BUCKET --server-side-encryption-configuration '{
+        "Rules": [
+            {
+                "ApplyServerSideEncryptionByDefault": {
+                    "SSEAlgorithm": "AES256"
+                }
+            }
+        ]
+    }'
+    aws s3api put-public-access-block \
+    --bucket $S3_BUCKET \
+    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 fi
 
 # Automatically create a deployment package for the AWS X-Ray Lambda Layer if it doesn't exist.
@@ -18,5 +30,5 @@ fi
 # Clear local pycache in lambda layer dependencies
 find lambda_layers -type d -name "__pycache__" -exec rm -r {} \;
 
-aws cloudformation package --template-file src/template.yaml --s3-bucket $S3_BUCKET --s3-prefix 'cfn' --output-template-file template-deploy.yaml
+aws cloudformation package --template-file src/template.yaml --s3-bucket $S3_BUCKET --output-template-file template-deploy.yaml
 aws cloudformation deploy --template-file template-deploy.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
--- lambda_layers/dependencies/python/logger.py
+++ lambda_layers/dependencies/python/logger.py
@@ -1,14 +1,3 @@
-"""
-This is a sample, non-production-ready template.
-
-© 2019 Amazon Web Services, In​c. or its affiliates. All Rights Reserved.
-
-This AWS Content is provided subject to the terms of the
-AWS Customer Agreement available at http://aws.amazon.com/agreement
-or other written agreement between Customer and either
-Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
-"""
-
 import logging
 
 
